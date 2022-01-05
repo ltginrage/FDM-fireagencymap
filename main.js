7df367cd6ef8e764c2859799920b4cc7f197ca82
Ethan Chapman <Eiim@users.noreply.github.com>
2022-01-05 16:22:27 -0500

    Load markers from data file

--- main.js
+++ main.js
@@ -4,5 +4,17 @@ document.addEventListener('DOMContentLoaded', (e) => {
 		maxZoom: 19,
 		attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 	}).addTo(map);
+	
+	// Add points
+	
+	var markers = L.markerClusterGroup();
+	
+	fetch("data.json").then(res => res.json()).then(data => {
+		for(point of data) {
+			markers.addLayer(L.marker([point.lat,point.long]).bindPopup(point.desc));
+		}
+		map.addLayer(markers);
+	});
+
 	console.log("mapped!");
 })
