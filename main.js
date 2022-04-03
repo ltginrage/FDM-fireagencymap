0d5cd70d19ad9fd1a1d1516bd3864df30a61a6be
ltginrage <97143303+ltginrage@users.noreply.github.com>
2022-04-03 12:59:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (e) => {
-	var map = L.map('map').setView([41.8, -87.8], 10);
+	var map = L.map('map').setView([41.79, -88.94], 3);
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
 		attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
